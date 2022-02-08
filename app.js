cb9b5711b54da9f4a1c80d80c8705e0d9045eb9e
mwright0481 <95373969+mwright0481@users.noreply.github.com>
2022-02-08 03:32:07 -0500

    json

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "index.html");
 
-const render = require("../lib/htmlRenderer");
+const render = require("./lib/htmlRender");
 
 const teamMembers = [];
 const emptyId = [];
